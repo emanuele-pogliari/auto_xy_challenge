989edd00d06ad687ef3e4752efb35f8c86975de4
Emanuele <manu.pogliari@gmail.com>
2025-02-13 15:54:21 +0100

    brand entity created

---
+++ src/Entity/Brand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BrandRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BrandRepository::class)]
+class Brand
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    #[ORM\Column(length: 255)]
+    private string $name;
+
+    #[ORM\OneToMany(targetEntity: CarModel::class, mappedBy: 'brand')]
+    private Collection $carModels;
+}
