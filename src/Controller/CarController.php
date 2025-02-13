fc0ac4652c042db5ddc6d84ac0a90cf6ad3933ac
Emanuele <manu.pogliari@gmail.com>
2025-02-13 21:04:10 +0100

    api route created and tested

---
+++ src/Controller/CarController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CarRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CarController extends AbstractController
+{
+    //api route
+    #[Route('/api/cars', name: 'api_cars_list', methods: ['GET'])]
+    public function getCars(CarRepository $carRepository): JsonResponse
+    {
+        $cars = $carRepository->findAll();
+
+        $data = [];
+        foreach ($cars as $car) {
+            $data[] = [
+                'id' => $car->getId(),
+                'year' => $car->getYear(),
+                'price' => $car->getPrice(),
+                'isAvailable' => $car->isAvailable(),
+                'model' => [
+                    'id' => $car->getModel()->getId(),
+                    'name' => $car->getModel()->getName(),
+                    'brand' => [
+                        'id' => $car->getModel()->getBrand()->getId(),
+                        'name' => $car->getModel()->getBrand()->getName(),
+                    ],
+                ],
+            ];
+        }
+
+        return $this->json($data);
+    }
+}    
+
