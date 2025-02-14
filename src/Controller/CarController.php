52318df375537ae2d6711941b0cebf5be7d42587
Emanuele <manu.pogliari@gmail.com>
2025-02-14 14:55:23 +0100

    cars api route refactored

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Car;
 use App\Repository\CarRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,74 +12,71 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 
-#[Route('/api')]
+#[Route('/api', name: 'api_')]
 class CarController extends AbstractController
 {
     public function __construct(
         private readonly CarRepository $carRepository
+
     ){}
 
-    //API ROUTES
-
-    //get all cars
     #[Route('/cars', name: 'api_cars_list', methods: ['GET'])]
-    public function getCars(): JsonResponse
+    public function index(EntityManagerInterface $entityManager): JsonResponse
     {
         return $this->json($this->resultsForApi($this->carRepository->findAll()));
 
     }
 
+    //helper method to format the API response
     private function resultsForApi(array $cars) : array
     {
-        return array_map([$this, 'singleCar'], $cars);
-    }
-    //returning data of the single car
-    private function singleCar(Car $car): array
-    {            
-        return [
-            'id' => $car->getId(),
-            'year' => $car->getYear(),
-            'price' => $car->getPrice(),
-            'isAvailable' => $car->isAvailable(),
-            'model' => [
-                'id' => $car->getModel()->getId(),
-                'name' => $car->getModel()->getName(),
-            ],
-            'brand' => [
-                'id' => $car->getModel()->getBrand()->getId(),
-                'name' => $car->getModel()->getBrand()->getName(),
-            ],
-        ];
+        return array_map(function ($car) {
+            return [
+                'id' => $car->getId(),
+                'brand' => [
+                    'id' => $car->getModel()->getBrand()->getId(),
+                    'name' => $car->getModel()->getBrand()->getName(),
+                ],
+                'model' => [
+                    'id' => $car->getModel()->getId(),
+                    'name' => $car->getModel()->getName(),
+                ],
+                'year' => $car->getYear(),
+                'price' => $car->getPrice(),
+                'isAvailable' => $car->isAvailable(),
+            ];
+        }, $cars);
+         
     }
 
 
     //WEB ROUTES
-    #[Route('/cars', name: 'car_list')]
-    public function carList(CarRepository $carRepository): Response
-    {
-        $cars = $carRepository->findAll();
+    // #[Route('/cars', name: 'car_list')]
+    // public function carList(CarRepository $carRepository): Response
+    // {
+    //     $cars = $carRepository->findAll();
     
-        return $this->render('cars/index.html.twig', [
-            'cars' => $cars
-        ]);
-    }
+    //     return $this->render('cars/index.html.twig', [
+    //         'cars' => $cars
+    //     ]);
+    // }
 
-    #[Route('/cars/add', name: 'car_add')]
-    public function addCar(): Response
-    {
-        return $this->render('cars/create.html.twig');
-    }
+    // #[Route('/cars/add', name: 'car_add')]
+    // public function addCar(): Response
+    // {
+    //     return $this->render('cars/create.html.twig');
+    // }
 
-    #[Route('/cars/{id}', name: 'car_show', methods: ['GET'])]
-    public function showCar(int $id): Response
-    {
-        return $this->render('cars/show.html.twig', ['id' => $id]);
-    }
+    // #[Route('/cars/{id}', name: 'car_show', methods: ['GET'])]
+    // public function showCar(int $id): Response
+    // {
+    //     return $this->render('cars/show.html.twig', ['id' => $id]);
+    // }
 
-    #[Route('/cars/{id}/edit', name: 'car_edit', methods: ['POST', 'GET'])]
-    public function editCar(int $id): Response
-    {
-        return $this->render('cars/edit.html.twig', ['id' => $id]);
-    }
+    // #[Route('/cars/{id}/edit', name: 'car_edit', methods: ['POST', 'GET'])]
+    // public function editCar(int $id): Response
+    // {
+    //     return $this->render('cars/edit.html.twig', ['id' => $id]);
+    // }
 }    
 
