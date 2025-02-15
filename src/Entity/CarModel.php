a6500f911bc3e1674bc365f7189133f4f101cb1d
Emanuele <manu.pogliari@gmail.com>
2025-02-15 11:13:01 +0100

    CarModel Entity created

---
+++ src/Entity/CarModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarModelRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CarModelRepository::class)]
+class CarModel
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(lenght: 255)]
+    private string $name;
+
+    #[ORM\ManyToOne(targetEntity: Brand::class, inversedBy: 'carModels')]
+    #[ORM\JoinColumn(nullable: false)]
+    private Brand $brand;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
