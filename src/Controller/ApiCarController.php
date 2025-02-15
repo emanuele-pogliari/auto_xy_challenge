72246b6f81b2279f1b49737bd93754bb8f3b9dd5
Emanuele <manu.pogliari@gmail.com>
2025-02-15 15:01:04 +0100

    api GET method completed (test passed)

--- src/Controller/ApiCarController.php
+++ src/Controller/ApiCarController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 
 use App\Entity\Car;
 use App\Entity\CarModel;
@@ -18,9 +20,17 @@ use App\Repository\CarRepository;
 #[Route('/api',)]
 class ApiCarController extends AbstractController
 {
+
+    public function __construct(
+        private CarRepository $carRepository,
+        private EntityManagerInterface $entityManager,
+    ){}
+
     #[Route('/cars', methods: ['GET'])]
     public function index(): JsonResponse
     {
+        return $this->json($this->resultsForApi($this->carRepository->findAll()));
+
     }
 
     #[Route('/car{id}', methods: ['GET'])]
@@ -41,6 +51,27 @@ class ApiCarController extends AbstractController
     #[Route('/car/{id}/delete', methods: ['DELETE'])]
     public function deleteCar(int $id): JsonResponse
     {
+    }
+
+    private function resultsForApi(array $cars) : array
+    {
+        return array_map(function ($car) {
+            return [
+                'id' => $car->getId(),
+                'brand' => [
+                    'id' => $car->getModel()->getBrand()->getId(),
+                    'name' => $car->getModel()->getBrand()->getName(),
+                ],
+                'model' => [
+                    'id' => $car->getModel()->getId(),
+                    'name' => $car->getModel()->getName(),
+                ],
+                'year' => $car->getYear(),
+                'price' => $car->getPrice(),
+                'isAvailable' => $car->isAvailable(),
+            ];
+        }, $cars);
+         
     }
 
 }
