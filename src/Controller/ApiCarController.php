c2b9977a372b2ea7215f678455be3be338e2da69
Emanuele <manu.pogliari@gmail.com>
2025-02-15 14:55:28 +0100

    import all needed dependencies

--- src/Controller/ApiCarController.php
+++ src/Controller/ApiCarController.php
@@ -5,16 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 use App\Entity\Car;
 use App\Entity\CarModel;
+use App\Entity\Brand;
 use App\Repository\CarRepository;
 
 
-#[Route('/api', name: 'api_')]
+#[Route('/api',)]
 class ApiCarController extends AbstractController
 {
-    #[Route('/cars', name: 'api_cars_list', methods: ['GET'])]
+    #[Route('/cars', methods: ['GET'])]
     public function index(): JsonResponse
     {
     }
